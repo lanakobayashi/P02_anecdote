17c4baf5c031545943b89c5a39dbfd0ac7d12498
Tim <timothychow5@gmail.com>
2018-03-06 20:34:35 -0800

    Validation debugging
    repeated use of IDs

--- public_html/js/hamburger.js
+++ public_html/js/hamburger.js
@@ -8,3 +8,12 @@ function myFunction() {
         x.className = "nav-container";
     }
 }
+
+function myFunction2() {
+    var x = document.getElementById("my-Nav");
+    if (x.className === "nav-container") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-container";
+    }
+}
